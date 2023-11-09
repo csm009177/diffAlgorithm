94fd03de89272ccc880c93c8e1d810f429981621
csm <csm009177@gmail.com>
2023-11-09 11:14:47 +0900

    success Q.one

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
 // main.js
+const fs = require('fs');
 
+fs.readFile('test.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error('파일을 읽어오는 동안 오류가 발생했습니다: ', err);
+    return;
+  }
+  //
+  const jsonData = JSON.parse(data);
+  console.log(jsonData);
+});
