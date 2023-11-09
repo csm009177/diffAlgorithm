7faba0b845b9c1b881f27ea0d8c465326f35c55b
csm <csm009177@gmail.com>
2023-11-09 11:11:53 +0900

    import json data from test.json

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
 // main.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MyComponent from '/comp.js';
 
-// React 컴포넌트를 루트 DOM 요소에 렌더링
-ReactDOM.render(<MyComponent />, document.getElementById('root'));
