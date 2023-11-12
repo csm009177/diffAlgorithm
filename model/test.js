4674658b4716f7c99638390fe1f1590a1f9dfb85
csm <csm009177@gmail.com>
2023-11-12 11:10:31 +0900

    put comment about input JSON data in difflogicTest fnc

--- model/test.js
+++ model/test.js
@@ -1,41 +1,43 @@
+// import 
 import fs from 'fs'
-// const path = require('path')
-
+// declare path 
 const inputPath = '../data/fromDB-data.json'
 const outputPath = '../data/differences.json'
 
+// diff logic Test
 function difflogicTest(inputJSONPath, outputJSONPath) {
-
+  // if not ends with '.json', throw new Error
   if (!inputJSONPath.endsWith('.json') || !outputJSONPath.endsWith('.json')) {
     throw new Error(`매개변수 ${inputJSONPath}, ${outputJSONPath}는 json 파일이 아닙니다.`);
   }
-
+  // init commonWord
   const commonWord = []
   const differentWord = []
+  // ReadFile / convert format / divide by state
   fs.readFile(inputJSONPath, 'utf8', (err, data) => {
     if (err) {
       console.error('error comment', err);
     }
+    // JSON 형식의 String을 JS 객체로 변환
     const inputJsonData = JSON.parse(data);
+    // access parsed JSON data 
     const operator = inputJsonData.operator 
     const operand = inputJsonData.operand
-
+    // split 
     const splitOperatorWord = operator.split(" ")
     const splitOperandWord = operand.split(" ")
-
-
+    // 각 요소에 대해 
     splitOperatorWord.forEach((element)=>{
+      // 요소가 요소를 포함하고 있으면(같은 것이 있으면) 
       if(splitOperandWord.includes(element)){
+        // commonWord에 푸시하고
         commonWord.push(element)
-      } else{
+      } else{ // 아니면 dfferentWord에 푸시
         differentWord.push(element)
       }
-    })
+    }) // print
     console.log(commonWord)
-    console.log(differentWord)
-    
-    
-    
+    console.log(differentWord)    
   });
 
   fs.readFile(outputJSONPath, 'utf8', (err, data) => {
@@ -58,9 +60,6 @@ function difflogicTest(inputJSONPath, outputJSONPath) {
    
     // console.log(outputJsonData.differenceWords.push(differentWord))
 
-   
-    
-    
   });
   
   let result = [];
