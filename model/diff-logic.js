5c7467aed2b77a85b0e988794305f107f3b7ad18
csm <csm009177@gmail.com>
2023-11-17 20:03:41 +0900

    nom1 is resolve

--- model/diff-logic.js
+++ model/diff-logic.js
@@ -1,4 +1,4 @@
-
+import fs from 'fs'
 /**
  * 
  * @param {JSON, Path} inputJSONdata 
@@ -10,12 +10,16 @@ export default function(inputJSONPath, outputJSONPath) {
   if (!inputJSONPath.endsWith('.json') || !outputJSONPath.endsWith('.json')) {
     throw new Error(`매개변수 ${inputJSONPath}, ${outputJSONPath}는 json 파일이 아닙니다.`);
   }
-  let result = {};
+  // * * 1. inputJSONdata, outputJSONdata를 읽어서 JSON 객체로 변환
+  const inputJSONdata = JSON.parse(fs.readFileSync(inputJSONPath, 'utf8'));
+
+  const outputJSONdata = JSON.parse(fs.readFileSync(outputJSONPath, 'utf8'));
+  let result = {inputJSONdata, outputJSONdata};
   /**
    * ? Q. JSON 파일을 아래의 5, 6번에 해당하는 로직 작성 후 JSON으로 저장
    * ? Q. 저장이 완료되면 초기화된 result에 객체를 리턴
    * 
-   * * 1. inputJSONdata, outputJSONdata를 읽어서 JSON 객체로 변환
+   
    * * 2. inputJSONdata, outputJSONdata의 value를 비교
    * * 3. outputJSONpath 매개변수의 key에 해당하는 정보를 저장
    * * 4. dirrences.json 파일에 필요한 상태값
